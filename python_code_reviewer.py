591d018a6eb30aa807cbaabda1d01a6ebf11df2a
aliensgroup04 <aliensgroup04@gmail.com>
2025-02-13 14:23:05 +0530

    Update python_code_reviewer.py

--- python_code_reviewer.py
+++ python_code_reviewer.py
@@ -10,7 +10,7 @@ Original file is located at
 import streamlit as st
 import google.generativeai as ai
 ai.configure(api_key='AIzaSyBMCc42a-cWcpnG1TfCC830kbHG20dAqpo')
-st.ballons()
+st.balloons()
 st.snow()
 sys_prompt="""You are a helpful Python application develpoer that allows users to submit their Python code for review and receive feedback on potential bugs along with suggestions
  for fixes. The application should be user-friendly, efficient, and provide accurate bug reports and fixed code snippets.In case if a student ask any question outside the python code scope,
